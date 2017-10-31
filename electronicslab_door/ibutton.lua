7387e85a6defb8e7dd1895eee41d30480a1efeb1
Hetti <git@thepeko.at>
2017-10-31 16:00:17 +0100

    cleanup prerequisite of master

--- electronicslab_door/ibutton.lua
+++ electronicslab_door/ibutton.lua
@@ -12,11 +12,11 @@ local pin=4 -- GPIO2
 local ledpin=1 -- GPIO5 (labeled as GPIO4 on the board!)
 ow.setup(pin)
 
-local database = dofile('database.lua')
 local learnMode = false
 local newName = ""
 local netState = 0
 local learnModeSocket = nil
+local usercount = -1
 
 
 local password = "CHANGETHIS"
@@ -24,6 +24,131 @@ local password = "CHANGETHIS"
 -- iButton LED
 gpio.mode(ledpin,gpio.OUTPUT)
 pwm.setup(ledpin,1,512)
+
+function list(c)
+    openDatabase()
+    local count = 0
+    local out = ""
+    while out ~= nil do
+        out = readDatabaseline()
+        local name = ""
+        if out ~= nil and count >= 0 then
+            --debug output
+            --print(out)
+            name = string.sub(out, 22)
+            name = string.sub(name, 1, -4)
+            print("User: "..name)
+            c:send("["..count.."] "..name.."\n")
+        end  
+        count = count+1
+    end
+	closeDatabase()  
+
+    usercount=count-1
+    print("usercount: "..usercount)
+    out = ""
+    c:send("> ")
+end
+
+function setUsercount()
+    openDatabase()
+    local count = 0
+    local out = ""
+    while out ~= nil do
+        out = readDatabaseline()
+        count = count+1
+    end
+    closeDatabase()  
+    usercount=count-1
+    print("Usercount set to "..usercount)
+end
+
+
+function openDatabase()
+	file.open( "database.lua", "r+" )
+end
+
+function closeDatabase()
+	file.close()
+end
+
+function rewriteDatabase(database)
+	file.open( "database.lua", "w+" )
+	file.write(database)
+	closeDatabase()
+end
+
+function appendDatabase(dataline)
+    file.open( "database.lua", "a+" )
+    file.write(dataline)
+    closeDatabase()
+end
+
+function readDatabaseline()	
+	--EOF gives nil value
+	return file.readline()	
+end
+
+function addtoDatabase(name, KeyID)
+    local writedb = ""
+
+    writedb = "['"..KeyID.."']='"..name.."',\n"
+    appendDatabase(writedb)
+    
+    usercount = usercount+1
+    print("Usercount after add: "..usercount)
+end
+
+function removeFromDatabase(number)
+	local count = 0
+	local line = ""
+	local newdb = ""
+	local deleted = ""
+    
+    openDatabase()
+    while count < usercount do
+        line = readDatabaseline()
+        if count == number then
+            deleted = line
+        end
+        
+        if line ~= nil and count ~= number then
+            newdb = newdb..line
+        end
+        count = count+1
+    end
+		closeDatabase()
+    deleted = string.sub(deleted, 22)
+    deleted = string.sub(deleted, 1, -4)
+    print("deleted following User: "..deleted.."\n")
+    
+    --rewrite Database without deleted user
+    rewriteDatabase(newdb)  
+    newdb = ""  
+		usercount = usercount-1
+		print("Usercount after del: "..usercount)
+end
+
+function checkID(ID)
+    local id2 = ""
+    local count = 0
+    local line = ""
+    print("GivenID: "..ID)
+    openDatabase()
+    while count < usercount do
+        line = readDatabaseline()
+        id2 = string.sub(line, 3, 17)
+        print ("found ID: "..id2)
+        if id2 == ID then
+            closeDatabase()
+            return true
+        end
+        count=count+1
+    end
+    closeDatabase()
+    print("count is "..count)
+    return false
+end
 
 function blink_led()
     pwm.setclock(ledpin,1)
@@ -52,19 +177,6 @@ function reverse_str2hex(str)
     return table.concat(result)
 end
 
-function saveDatabase()
-    file.open("database.lua", "w+")
-    file.writeline("return {")
-
-    for k, v in pairs(database) do
-        local escaped,count = string.gsub(v, "'", "\\'")
-        file.writeline(string.format("['%s']='%s',", k, escaped))
-    end
-
-    file.writeline("}")
-    file.close()
-end
-
 tmr.alarm(3, 100, 1, function()
     if not isDoorMoving() then
         local addr
@@ -78,16 +190,14 @@ tmr.alarm(3, 100, 1, function()
             if (crc == addr:byte(8)) then
                 print("CRC OK")
                 if learnMode then
-                    database[id] = newName
-                    saveDatabase()
+					addtoDatabase(newName, id)
                     learnMode = false
                     netState = 0
-                    learnModeSocket:send("Registered new device " .. id .. "\nHave a nice day!\n> ")
+                    learnModeSocket:send("Registered new device\nHave a nice day!\n> ")
                     blink_led()
                 else
-                    name = database[id]
-                    if name then
-                        print("Detected user " .. name)
+                    if checkID(id) then
+                        print("Detected legit user with ID "..id)
                         blink_led_stop()
                         doorUp(function()
                             blink_led()
@@ -104,16 +214,13 @@ end)
 local stateMachine = {
     [0] = function(c, line)
         if line == password then
-            c:send("Please enter name:\n> ")
+            c:send("Please enter mode [add or del]:\n> ")
             netState = 1
         elseif line == "help" then
             c:send("Help is for the weak.\n> ")
         elseif line == "list" then
             c:send("Known users:\n")
-            for k,v in pairs(database) do
-                c:send(v .. "\n")
-            end
-            c:send("> ")
+            list(c)
         elseif line == "exit" or line == "logout" or line == "logoff" then
             c:send("Thank you for flying with Metalab Airlines!\n")
             c:close()
@@ -128,20 +235,53 @@ local stateMachine = {
             c:send("Unrecognized command.\n> ")
         end
     end,
+
     [1] = function(c, line)
+        if line == "add" then
+            c:send("Please enter name:\n>")
+            netState = 2
+        elseif line == "del" then
+            c:send("Which user to delete? Choose a Number\n")
+            list(c)
+            netState = 3
+        else
+            c:send("Unrecognized command.\n> ")
+            netState = 0
+        end
+    end,
+       
+    [2] = function(c, line)
         newName = line
         learnMode = true
         c:send("Learn mode activated. Please connect new iButton device.\n")
         learnModeSocket = c
-        blink_led_fast()
+        blink_led_fast()        
     end,
-    [2] = function(c, line)
+    
+    [3] = function(c, line)
+        local delindex = tonumber(line)
+        
+        if delindex == nil then
+            c:send("Please enter valid Indexnumber!\n")
+            list(c)
+            
+        elseif delindex >= 0 and delindex < usercount then
+           c:send("Deleting Number "..delindex.."\n> ")
+           removeFromDatabase(delindex)
+           c:send("User deleted!\n> ")
+           netState = 0
+        else
+            print("delindex was :"..delindex)
+            c:send("Index out of bound exception, try again!\n")
+            list(c)    
+        end
     end,
 }
 
 sv=net.createServer(net.TCP,40)
 sv:listen(1337,function(c)
     c:send("Metalab Electronics Lab Control. Authorized personell only.\n> ")
+		netState = 0
     local buffer = ""
     c:on("receive", function(c, pl)
         buffer = buffer .. pl
--- scannerroom_door/init.lua
+++ scannerroom_door/init.lua
@@ -14,3 +14,4 @@ wifi.sta.setip({ip="10.20.30.18",netmask="255.255.0.0",gateway="10.20.30.1"})
 
 dofile("doorcontrol.lua")
 dofile("ibutton.lua")
+setUsercount()
